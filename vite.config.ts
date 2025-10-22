8f670cf8504f0fdd5943f1ca3988a6c075180c68
Praneesh Reddy Gunukula <praneeshgunukula@gmail.com>
2025-10-22 17:31:31 +0000

    change in cofig file

--- vite.config.ts
+++ vite.config.ts
@@ -10,9 +10,10 @@ export default defineConfig(({ mode }) => {
         host: '0.0.0.0',
       },
       plugins: [react()],
+      build: {
+        outDir: 'docs', // Build output will go here
+      },
       define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
       },
       resolve: {
         alias: {
